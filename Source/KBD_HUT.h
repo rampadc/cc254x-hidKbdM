ad417f539698b41a34834593238776c070809ba5
rampadc <congnguyen1006@gmail.com>
2014-07-31 18:18:39 +0930

    Update KBD_HUT.h
    Added license

--- Source/KBD_HUT.h
+++ Source/KBD_HUT.h
@@ -3,6 +3,18 @@
  *
  *  Created on: 08/09/2013
  *      Author: CONG
+ * Copyright 2014 Cong Nguyen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and limitations under the License. 
  */
 
 #ifndef KBD_HUT_H_
