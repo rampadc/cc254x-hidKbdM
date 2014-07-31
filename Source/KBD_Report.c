56ace6deee07dcda9581a9d62f284fbfbd6cd6f4
rampadc <congnguyen1006@gmail.com>
2014-07-31 18:21:17 +0930

    Update KBD_Report.c
    Updated file description and added license

--- Source/KBD_Report.c
+++ Source/KBD_Report.c
@@ -1,3 +1,22 @@
+/* 
+ * File name: KBD_Report.c
+ * Description: Keyboard report builder and handler. Based on Texas Instruments SLAA514
+ *              Implemented to send keyboard reports over BLE with CC2540
+ *
+ * Copyright 2014 Cong Nguyen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and limitations under the License. 
+*/
+
 #include "KBD_Report.h"
 #include "hiddev.h"
 
